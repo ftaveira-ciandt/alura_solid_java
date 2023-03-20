017b1b56915e5bc84f488f7c50c2d1843898a803
Francisco Taveira <ftaveira@ciandt.com>
2023-03-20 16:01:13 -0300

    criado metodo para ver a data do ultimo reajustes

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -2,14 +2,20 @@ package br.com.alura.rh.service;
 
 public class ReajusteService {
 
-    public void reajustarSalarioDoFuncionario(Funcioanrio funcioanrio, BigDecimal aumento) {
-        BigDecimal salarioAtual = funcioanrio.getSalario();
+    public void reajustarSalarioDoFuncionario(Funcioanrio funcionario, BigDecimal aumento) {
+        BigDecimal salarioAtual = funcionario.getSalario();
         BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
             throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
         }
+
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
         BigDecimal salarioReajustado = salarioAtual.add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
-    }
+        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+        if (mesesDesdeUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes nao pode ser menor do que 6 meses");
+        }
     }
 }
