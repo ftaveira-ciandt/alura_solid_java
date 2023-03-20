75a842455ebc6f7df93af6e33782758016c90c05
Francisco Taveira <ftaveira@ciandt.com>
2023-03-20 16:30:56 -0300

    criado interface validacaoReajuste

--- src/br/com/alura/rh/service/ValidacaoPeriocidadeReajuste.java
+++ src/br/com/alura/rh/service/ValidacaoPeriocidadeReajuste.java
@@ -1,6 +1,6 @@
 package br.com.alura.rh.service;
 
-public class ValidacaoPeriocidadeReajuste {
+public class ValidacaoPeriocidadeReajuste implements ValidacaoReajuste{
 
     public void validar(Funcionario funcionario, BigDecimal aumento) {
     LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
